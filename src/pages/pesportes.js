e8286ab5bafd49076e199ba35a1d13a95f3b16da
Aquino050 <vitoria.aquino+github@estudante.ifms.edu.br>
2024-11-29 11:50:02 +0000

    aula 29/11

--- src/pages/pesportes.js
+++ src/pages/pesportes.js
@@ -1,16 +1,43 @@
 import { Container } from "react-bootstrap";
 import Menu from "./modulos/navbar";
 import Footer from "./modulos/footer";
+import Table from 'react-bootstrap/Table';
 
 export default function Pesportes() {
     return <>
         <Menu />
         <Container>
             <Container className="border border-sm border-success rounded-pill text-center col-sm-6">
-            Lista de produtos de esportes
+                Lista de produtos de esportes
             </Container>
             <Container>
-             Os Produtos
+                <Table striped bordered hover>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Produto</th>
+                            <th>Categoria</th>
+                            <th>Descrição</th>
+                            <th>Preço</th>
+
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        <tr>
+
+                        </tr>
+                        <tr>
+                           
+                        </tr>
+                    </tbody>
+                </Table>
             </Container>
         </Container >
         <Footer />
